5832b61a8dfcb41a4d7d92dae2e44d7cf17160a4
ChrisL <caleedev@gmail.com>
2024-11-03 21:51:41 -0500

    acompleted debugging and functionality testing

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
 /*Button Examples.............*/
-const buttonElements = document.querySelectorAll('button');
-const spanElements = document.querySelectorAll(`.count`);
-const spanStock = document.querySelectorAll(`.inventory`)
-const moneyElement = document.querySelector('#money');
+const blockElement = document.querySelector('#playerBlock');
+const playerHPElement = document.querySelector('#playerHP');
+const enemyAtkElement = document.querySelector('#enemyAtk');
+const enemyNameElement = document.querySelector('#enemyName');
+const enemyHPElement = document.querySelector('#enemyHP');
+const cardElements = document.querySelectorAll('.cards');
+const energyElement = document.querySelector('#energy');
+const drawPileElement = document.querySelector('#drawPile');
+const discPileElement = document.querySelector('#discPile');
+const endButtonElement = document.querySelector('#endTurn');
+
+//test button
+const testButtonElement = document.querySelector('#test');
+
+/*Same rules as css, # for ID, . for class, or spell the element*/
 
 
 const game = {
     monster: [
         {name: 'Bug', hp: 20, baseAtk: 1},
-        {name: 'Code Moneky', hp: 40, baseAtk: 5}
+        {name: 'Code Monkey', hp: 40, baseAtk: 5}
     ],
-    display: {
-        Draw: 0,
-        Disc: 0
-    },
+    currentEnemy: 0,
     card: [
-        {id: 1, name: 'de-bug', text: 'deal 5 damage', dmg: 5, block: 0},
-        {id: 2, name: 'NULL-ify', text: 'gain 6 block this turn', dmg: 0, block: 6}
+        {id: 0, name: 'de-bug', text: 'deal 5 damage', dmg: 5, block: 0},
+        {id: 1, name: 'NULL-ify', text: 'gain 6 block this turn', dmg: 0, block: 6}
     ],
     hand:[     
     ],
-    deck:[1,1,1,1,1,2,2,2,2,2],
+    deck:[0,0,0,0,0,1,1,1,1,1],
     drawPile:[],
     discPile:[],
-    player: {name: '', hp: 50, blkAmount: 0, energy: 3 },
+    player: {name: '', hp: 50, blkAmount: 0, energy: 3},
     enemy: {name: '', hp: 999, baseAtk: 50, dmg: 0},
-    currentCard = null
 
 
+}
+
+//initialize monster 
+function genEnemy(x){
+    game.enemy.name = game.monster[x].name
+    game.enemy.hp = game.monster[x].hp
+    game.enemy.baseAtk = game.monster[x].baseAtk
 }
 
 function randomBoi(max) {
@@ -38,140 +52,181 @@ function randomBoi(max) {
     Thus simulating a shuffle effect. This is referred to as a Fisher-Yates shuffle.*/
 function shuffle(myArray) {
     for(let i=0; i<myArray.length; i++) {
-        j = randomBoi(myArray.length)
-        myArray[i], myArray[j] = myArray[j], myArray[i]
+        j = randomBoi(myArray.length);       
+        [myArray[i], myArray[j]] = [myArray[j], myArray[i]]
     }
 }
 
 function draw(){
     //shuffle discard into drawpile if drawpile is empty
     if (game.drawPile.length === 0) {
-        for(let i = 0; i<game.discPile.length; i++){
-        game.drawPile[i]=game.discPile.pop()}
+        console.log('recycling discard to draw');
+        
+        // for(let i = 0; i<game.discPile.length; i++){  fun bug
+            while (game.discPile.length>0) {
+            console.log('Popping 1 to draw');
+            console.log(`Discpile: ${game.discPile.length}`);         
+            
+        game.drawPile.push(game.discPile.pop())}
         shuffle(game.drawPile)
         }
-    game.hand.push = game.drawPile.pop
+        console.log('Drawing 1');
+    game.hand.push(game.drawPile.pop())
     
 }
 //draw 4 cards at start of game/turn
 function drawHand(){
-    for(let i=0; i<4; i++){
+    for(let i=0; i<4; i++){    
         draw()
     }
 }
-
 //discards hand
 function discard(){
  while(game.hand.length > 0){
-    game.discPile.push = game.hand.pop
+    game.discPile.push(game.hand.pop())
  }
 }
-
 //monster attack base plus the below number (5-15)
 function getAtk() {
     return (randomBoi(11)+5)
 }
-
 function begin() {
-    //Load deck to draw pile
+    genEnemy(game.currentEnemy)
+    //Load deck to draw pile 
     for(let i=0; i<game.deck.length; i++){
-        game.drawPile[i]=game.deck[i]
+        game.drawPile.push(game.deck[i])   
     }
     //shuffle
     shuffle(game.drawPile)
+    // console.log(game.drawPile);
     drawHand()
+    // console.log(game.hand);
+    
     //set enemy damage for next turn
     game.enemy.dmg = (game.enemy.baseAtk+getAtk())
+    pushDisplay()
 }
-
 //whether the card is an attack or block, this should
 //perform its function
-function playCard(x) {
-    if(game.player.energy > 0 && game.card[x].dmg < game.enemy.hp){
-    game.enemy.hp - game.card[x].dmg
-    game.player.blkAmount+game.card[x].block
+function playCard(x,y) {
+
+    if(game.player.energy > 0 && game.card[y].dmg < game.enemy.hp){
+        // console.log(`Damage: ${game.card[y].dmg}, Block: ${game.card[y].block}`); 
+    game.enemy.hp = (game.enemy.hp - game.card[y].dmg)
+    // console.log(`new enemy hp: ${game.enemy.hp}`);
+    game.player.blkAmount = (game.player.blkAmount+game.card[y].block)
+    // console.log(`new player block: ${game.player.blkAmount}`);
     game.player.energy--
+    // console.log(`pushing card id: ${game.card[y].id}`);
+    game.discPile.push(game.card[y].id)//put copy of card in discard pile
+    // console.log(`Splicing hand position: ${game.hand[x]}`);
+    game.hand.splice(x,1)//remove card from hand
+    // console.log(game.discPile);
     } else if(game.player.energy === 0) {
         alert("No Mana Error")
     } else {
         //Enhancement - Make a variable to track which monster we are on and increment it when defeating.
         //Then we can change these value assignments below to match
-        alert(`You defeated the ${game.enemy.name}! /n After catching your breath you head to the next room where you find a new enemy`)
-        game.enemy.name = game.monster[1].name
-        game.enemy.baseAtk = game.monster[1].baseAtk
-        game.enemy.hp = game.monster[1].hp
+        alert(`You defeated the ${game.enemy.name}! You win!`)
+        genEnemy(1)
         game.enemy.dmg = (game.enemy.baseAtk+getAtk()) 
     }
+    pushDisplay()
 }
 
 
+function pushDisplay() {
+    blockElement.textContent = game.player.blkAmount
+    playerHPElement.textContent = game.player.hp
+    enemyAtkElement.textContent = game.enemy.dmg
+    enemyNameElement.textContent = game.enemy.name
+    enemyHPElement.textContent = game.enemy.hp
+    energyElement.textContent = game.player.energy
+    drawPileElement.textContent = game.drawPile.length
+    discPileElement.textContent = game.discPile.length
 
-//XXXXXXXXXXXXXCreate a catch case for player death and monster defeat 
+
+    // console.log(cardElements[0].textContent);
+    // console.log(game.hand[0]);
+        
+    // console.log(`Hand Length is: ${game.hand.length}`);
+    
+    for(let i = 0; i < game.hand.length || i < 4; i++) {
+        //console.log('HELLO');
+        //console.log(cardElements);
+        //console.log(cardElements[i]);
+        // console.log(`i:${i}`);
+        // console.log(`game.hand[i]:${game.hand[i]}`);
+        
+        
+       cardElements[i].textContent = game.hand[i] //CHECK MEEEE
+    }
+
+}
+
 //runs when player end their turn
 function battle() {
+
+    console.log(`enemy dmg: ${game.enemy.dmg}, player block: ${game.player.blkAmount}`);
+    
     //subtract block from dmg
     if(game.enemy.dmg >= game.player.blkAmount) {
         game.enemy.dmg = game.enemy.dmg-game.player.blkAmount
     } else {
         game.enemy.dmg = 0
     }
+
     //deals remaining dmg to player
     if(game.enemy.dmg > 0 && game.enemy.dmg < game.player.hp) {
-        game.player.hp-game.enemy.dmg
-    } else {alert("Game Over: You were defeated")
+        game.player.hp=(game.player.hp-game.enemy.dmg)
+    } else if(game.enemy.dmg === 0) {
+        console.log('Do Nothing');
+        
+    } else {
+        alert("Game Over: You were defeated")
         //XXXXXXXXXX Disable buttons
     }
     //cleanup phase, clear values
     game.player.blkAmount = 0
     game.enemy.dmg = 0
     //discard remaining cards
-    for(let i=0; i< game.hand.length; i++){
-        game.discPile.push = game.hand.pop
-    }
+    
+    
+    discard()
+    console.log(`discard size: ${game.discPile.length}`);
     //draw new hand
     drawHand()
-
-
-    
-
-
+    //reset player energy
+    game.player.energy = 3;
     game.enemy.dmg = (game.enemy.baseAtk+getAtk())
 }
 
 
+/*--------------------------PROGRAM RUNS-----------------------------*/
+// console.dir(cardElements[0]);
 
+//verifying that the hand HTML elements will alway equal the array position of the card they represent
+// cardElements.forEach(element => {
+//     console.log(element.id)   
+// });
 
+cardElements.forEach((card) => {
+    card.addEventListener(`click`, () => {
+        console.log(`passing cardid: ${card.id}, passing cardtext: ${card.textContent}`);
+        
+        playCard(card.id, card.textContent)
+    })
 
+})
 
+// testButtonElement.addEventListener(`click`, () => {
+    begin()
+// })
+
+endButtonElement.addEventListener(`click`, () => {
+    console.log('PUSH ME');
     
-    buttonElements.forEach((button) => {
-
-        button.addEventListener(`click`, (event) => {
-    
-            //because we gave IDs to rows, we are using IDs to pull 
-            //their position
-            //when we query them as an array (e.g. querySelectorAll)
-            const rowID = event.target.parentElement.id.split('')[1];
-    
-
-const gameo = {
-    ingredients: [
-       {name: 'sauce', stock: 5, price: 3},
-       {name: "meat", stock: 5, price: 5},
-        {name: "dough", stock: 5, price: 2},
-        {name: "oregano", stock: 5, price: 1},
-        {name: "noodles", stock: 5, price: 2},
-        {name: "bread", stock: 5, price: 3}
-    ],
-    foodOrders: [0],
-    meals: [
-        {name: "pizza", price: 10, sold: 0},    
-        {name: "sub", price: 10, sold: 0},
-        {name: "pasta", price: 10, sold: 0}
-    ],
-    cartArray: [0,0,0,0,0,0],
-    money: 100,
-    day: 0
-
-
+    battle()
+    pushDisplay()
+})
 
